16d7b7d6f08702c81853e55ac223aa2aff6aa25b
Jørgen <jorgen.s.baumann@gmail.com>
2021-10-22 15:55:25 +0200

    all_result displayed as list, but table not visible. CSS to be fixed.

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 from django.utils import timezone
 
 from .models import Question, Choice
@@ -16,7 +16,7 @@ class AllResults(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-class HomeView(generic.DetailView):
+class HomeView(View):
     template_name = 'polls/home.html'
 
 class IndexView(generic.ListView):
